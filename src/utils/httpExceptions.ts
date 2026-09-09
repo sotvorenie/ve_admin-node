564055be5c21eb1bf17e8f0a90464cf6c728bfa0
vitalik abrosimov <vitalikabrosimov00@gmail.com>
2026-09-09 12:05:34 +0300

    добавлен code для jwtException

--- src/utils/httpExceptions.ts
+++ src/utils/httpExceptions.ts
@@ -1,16 +1,18 @@
 export class AppError extends Error {
     status: number
     detail: string
+    code?: string | undefined
 
-    constructor(status: number, detail: string) {
+    constructor(status: number, detail: string, code?: string) {
         super(detail);
         this.status = status;
         this.detail = detail;
+        this.code = code;
         this.name = 'AppError';
     }
 }
-export const HttpError = (status: number, detail: string) => {
-    return new AppError(status, detail)
+export const HttpError = (status: number, detail: string, code?: string) => {
+    return new AppError(status, detail, code)
 }
 
 // signal
@@ -20,14 +22,13 @@ export const abortedException = HttpError(408, "Запрос отменен");
 export const dbException = HttpError(500, "Ошибка БД");
 
 // авторизация и токен
-export const jwtException = HttpError(401, "Не удалось валидировать токен");
+export const jwtException = HttpError(401, "Не удалось валидировать токен", 'JWT_INVALID');
 export const registrationException = HttpError(409, "Пользователь с таким логином уже существует");
 export const authException = HttpError(401, "Неверное имя или пароль");
 
 // редактирование данных пользователя
 export const emptyUserDataException = HttpError(400, "Неверные данные пользователя");
 export const duplicationPasswordException = HttpError(400, "Новый пароль должен отличаться от текущего");
-export const duplicationLoginException = HttpError(400, "Данный логин уже занят!!");
 
 // загрузка файлов
 export const photoFormatException = HttpError(400, "Неподдерживаемый формат фото");
